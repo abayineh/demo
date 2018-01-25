e996d5dfa491dd5f5b8d48cad6b82e8f2a4ae362
tyirga <esyan4@gmail.com>
2018-01-25 08:54:48 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Scanner;
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String showIndex()
+    {
+
+        String UserName="";
+                Scanner keyboard=new Scanner(System.in);
+                UserName = keyboard.nextLine();
+                String greet ="Hello "+ UserName;
+
+                System.out.println(greet);
+        return greet;
+    }
+}
